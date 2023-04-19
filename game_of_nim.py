034a0149e504a80d884124c5c2251cee14053fcf
Nicholas Caro <nicholascaro@csu.fullerton.edu>
2023-04-18 19:52:31 -0700

    submission

--- game_of_nim.py
+++ game_of_nim.py
@@ -1,5 +1,7 @@
 from games import *
 
+# Group Members: Nicholas Caro, Chandra Lindy, Dilhan Franco
+
 
 class GameOfNim(Game):
     # takes the initial board position
@@ -10,34 +12,49 @@ class GameOfNim(Game):
         moves = [(i, j+1) for i, val in enumerate(board)
                  if val != 0 for j in range(val)]
         self.initial = GameState(
-            to_move='MAX', utility=0, board=self.board, moves=moves)
+            to_move='Min', utility=0, board=self.board, moves=moves)
 
     # returns the new state reached from the given state
     # and the given move. Assume the move is a valid move.
     # Note that the state for a multiplayer game also includes the
     # player whose turn it is to play
-    def result(state, move):
-        raise NotImplementedError
+    def result(self, state, move):
+        row, sticks = move
+        board = state.board.copy()
+        board[row] -= sticks
+        moves = [(i, j+1) for i, val in enumerate(board)
+                 if val != 0 for j in range(val)]
+        return GameState(to_move=('Min' if state.to_move == 'Max' else 'Max'),
+                         utility=0,
+                         board=board, moves=moves)
 
     # returns a list of valid actions in the given state.
     # This is easy if you generate the list of valid moves
     # when a child state is created.
-    def actions(state):
-        raise NotImplementedError
+    def actions(self, state):
+        valid_actions = state.moves.copy()
+        return valid_actions
 
     # returns True if the given state represents the end of a game
-    def terminal_test(state):
-        raise NotImplementedError
+    def terminal_test(self, state):
+        if self.actions(state=state) == []:
+            return True
+        else:
+            return False
 
     # returns +1 if MAX wins, -1 if MIN wins (the "names" of the x
     # players don't matter as long as they are distinct)
-    def utility(state, player):
-        raise NotImplementedError
+    def utility(self, state, player):
+        if self.terminal_test(state) == True and player == "Max":
+            return 1
+        else:
+            return -1
 
     # returns the player whose turn it is to move.
     # The default implementation in abstract class Game should be sufficient.
-    def to_move(state):
-        raise NotImplementedError
+    def to_move(self, state):
+        player = state.to_move
+        return player
 
 
 if __name__ == "__main__":
